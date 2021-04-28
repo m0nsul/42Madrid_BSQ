a589b74daa85619eae09ade9f5b2ad0622cf6520
Alejandro Martinez alonso <alemarti@c3r1s6.42madrid.com>
2021-04-28 13:33:53 +0200

    Creacion nuevo check_line

--- ex00/src/ft_check_first_line.c
+++ ex00/src/ft_check_first_line.c
@@ -1,24 +1,21 @@
+#include "ft_str.h"
+
 int	ft_check_first_line(char *str)
 {
-	int		res;
-	int		count;
-	int		i;
+	int	i;
 
-	i = 0;
-	count = 0;
-	res = 0;
-	while (str[i] >= '0' && str[i] <= '9')
-		i++;
-	if (i < 1)
+	i = -1;
+	if ((ft_strlen(str) < 4)
+		|| str[ft_strlen(str) - 1] == str[ft_strlen(str) - 2]
+		|| str[ft_strlen(str) - 2] == str[ft_strlen(str) - 3]
+		|| str[ft_strlen(str) - 3] == str[ft_strlen(str) - 1])
 		return (0);
-	while (str[i] >= 32 && str[i] < 127)
+	while (++i < ft_strlen(str))
 	{
-		count++;
-		i++;
+		if (str[i] < 32 || str[i] > 126)
+			return (0);
+		if (i < ft_strlen(str) - 3 && (str[i] < '0' || str[i] > '9'))
+			return (0);
 	}
-	if (str[i] == str[i - 1] || str[i] == str[i - 2]
-		|| str[i - 1] == str[i - 2] || count != 3 || str[i] != 0)
-		return (0);
-	else
-		return (1);
+	return (1);
 }
